341a4e7f535887f5c1159be00423c6d94d2670a0
dschang116 <derekchang@dhcp-10-250-46-134.harvard.edu>
2020-10-23 17:42:51 -0400

    edited G20_countries

--- shiny_app/app.R
+++ shiny_app/app.R
@@ -12,39 +12,61 @@ library(tidyverse)
 library(readxl)
 library(ggplot2)
 
-# Load in IMF rGDP data.
+# Load in IMF rGDP data. Rename first column to country 
 # Drop NA values.
 
 imf_rGDP_growth <- read_excel("raw_data/imf-dm-export-20201014.xls") %>%
+    rename(country = "Real GDP growth (Annual percent change)") %>%
     drop_na()
 
 # Replace values of no data with NA.
 
 imf_rGDP_growth[imf_rGDP_growth == "no data"] <- NA
 
-# Rename first column to country and change column types 
 
-# https://www.gov.uk/eu-eea
+# Rename China and South Korea.
+
+imf_rGDP_growth$country[imf_rGDP_growth$country == 
+                            "China, People's Republic of"] <- "China"
+imf_rGDP_growth$country[imf_rGDP_growth$country == 
+                            "Korea, Republic of"] <- "Korea"
+
+# Filter to G20 countries and order alphabetically. 
+# Change first column country as factor and rest as numeric column type. 
+
+G20_countries <- c("Argentina", "Australia", "Brazil", "Canada", 
+                   "China", "France", 
+                   "Germany", "India", "Indonesia", "Italy", "Japan", 
+                   "Korea", "Mexico", "Russian Federation", 
+                   "Saudi Arabia", "South Africa", "Turkey", 
+                   "United Kingdom", "United States", "European Union",
+                   "Austria", "Belgium", "Bulgaria",  "Croatia",  
+                   "Republic of Cyprus", "Czech Republic", "Denmark", 
+                   "Estonia", "Finland", "Greece", 
+                   "Hungary", "Ireland", "Latvia", "Lithuania", 
+                   "Luxembourg", "Malta", "Netherlands", "Poland", 
+                   "Portugal", "Romania", "Slovakia", "Slovenia", 
+                   "Spain", "Sweden") %>%
+    sort()
 
 G20_rGDP_growth <- imf_rGDP_growth %>% 
-    rename("country" = "Real GDP growth (Annual percent change)") %>% 
-    filter(country %in% c("Argentina", "Australia", "Brazil", "Canada", 
-                          "China, People's Republic of", "France", 
-                          "Germany", "India", "Indonesia", "Italy", "Japan", 
-                          "Korea, Republic of", "Mexico", "Russian Federation", 
-                          "Saudi Arabia", "South Africa", "Turkey", 
-                          "United Kingdom", "United States", "European Union")) %>% 
+    filter(country %in% G20_countries) %>% 
     mutate_at(1, as.factor) %>%
-    mutate_at(2:46, as.numeric)
+    mutate_at(2:47, as.numeric) %>% 
+    select(1:41)
 
 # Define UI for application that draws a histogram
 ui <- navbarPage(
-    "Final Project Title",
-    tabPanel("Model",
+    "Factors of Economic Growth",
+    tabPanel("Distribution by Year",
              fluidPage(
                  
                  # Application title
-                 titlePanel("IMF Real GDP Growth Data in 1980"),
+                 titlePanel("Real GDP Growth Distribution by Year"),
+                 
+                 selectInput("year", "Choose a year:",
+                             list(`Year` = c(seq(1980,2019)))
+                 ),
                  
                  # Sidebar with a slider input for number of bins 
                  sidebarLayout(
@@ -52,36 +74,39 @@ ui <- navbarPage(
                          sliderInput("bins",
                                      "Number of bins:",
                                      min = 1,
-                                     max = 50,
-                                     value = 30)
+                                     max = 20,
+                                     value = 10)
                      ),
                      
+                     
+                     
                      # Show a plot of the generated distribution
-                     mainPanel(
-                         plotOutput("distPlot")
-                     )
+                     mainPanel(plotOutput("GDPDistributionByYear"))
                  )
+                 
              )),
-    tabPanel("Graph",
+    tabPanel("Real GDP Growth by Country",
              fluidPage(
                  
                  # Application title
-                 titlePanel("Checkboxes"),
+                 titlePanel("G20 Countries and European Union"),
+                 
                  
                  # Sidebar with a slider input for number of bins 
                  sidebarLayout(
                      sidebarPanel(
-                         checkboxGroupInput("checkGroup", 
-                                            h3("Checkbox group"), 
-                                            choices = list("Colombia" = "col", 
-                                                           "Japan" = "jpn", 
-                                                           "Germany" = "germ"),
-                                            selected = 1)),
+                         checkboxGroupInput("country", 
+                                            ("Pick a country:"), 
+                                            choiceNames =
+                                                G20_countries,
+                                            choiceValues =
+                                                G20_countries,
+                                            selected = "United States"
+                         )),
                      
                      # Show a plot of the generated distribution
-                     mainPanel(
-                         plotOutput("checkPlot")
-                     )
+                     mainPanel(plotOutput("GDPByCountry")),
+                     
                  )
              )),
     tabPanel("Discussion",
@@ -101,22 +126,59 @@ ui <- navbarPage(
              h3("About Me"),
              p("My name is Derek Chang, and I am a first-year undergraduate at 
              Harvard University studying Economics.
-             You can reach me at dschang@college.harvard.edu.")))
+             You can reach me at dschang@college.harvard.edu.")),
+    
+    tags$head(
+        tags$style(type = 'text/css', 
+                   HTML('.navbar { background-color: aqua;}
+                          .navbar-default .navbar-brand{color: white;}
+                          .tab-panel{ background-color: red; color: white}
+                          .navbar-default .navbar-nav > .active > a, 
+                           .navbar-default .navbar-nav > .active > a:focus, 
+                           .navbar-default .navbar-nav > .active > a:hover {
+                                color: #555;
+                                background-color: blue;
+                            }')
+        )
+    )
+    
+)
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- G20_rGDP_growth[[2]]
+    
+    output$GDPDistributionByYear <- renderPlot({
+        year <- as.numeric(input$year)
+        plotData <- G20_rGDP_growth[[year - 1980 + 2]]
         
-        bins <- seq(min(x, na.rm = TRUE), max(x, na.rm = TRUE), 
+        bins <- seq(min(plotData, na.rm = TRUE), max(plotData, na.rm = TRUE), 
                     length.out = input$bins + 1)
-
+        
         # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    }) 
-    output$checkPlot <- renderPlot({print(input$checkGroup)})
+        hist(plotData, breaks = bins, col = 'darkmagenta', border = 'white',
+             main = paste("Real GDP Growth Distribution for Year", year),
+             xlab = "GDP Growth %",
+             ylab = "Number")
+    })
+    
+    output$GDPByCountry = renderPlot({
+        countrySelected <- input$country
+        
+        plotData <- G20_rGDP_growth %>% filter(country %in% countrySelected) %>%
+            pivot_longer(cols = - country,
+                         names_to = "year", 
+                         values_to = "gdp")
+        
+        ggplot(plotData, aes(x = year, y = gdp, 
+                             group = country, color = country)) +
+            geom_line() +
+            labs (x = "Year", y = "GDP Growth %", color = "Country", 
+                  title = paste("GDP Growth for", 
+                                paste(countrySelected, collapse = ', '), 
+                                "through years")) +
+            theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+        
+    })
 }
 
 # Run the application 
